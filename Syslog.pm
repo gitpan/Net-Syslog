56367ac23b62b6f08a649941cf9094ecbaa0f3af
Les Howard <lhoward@spamcop.net>
2010-10-23 15:21:29 +0000

    Import of LHOWARD/Net-Syslog-0.04 from CPAN.
    gitpan-cpan-distribution: Net-Syslog
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         LHOWARD/Net-Syslog-0.04.tar.gz
    gitpan-cpan-author:       LHOWARD
    gitpan-cpan-maturity:     released

--- Syslog.pm
+++ Syslog.pm
@@ -1,108 +1,129 @@
 package Net::Syslog;
 
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+use vars qw($VERSION);
+use warnings;
+use strict;
 use IO::Socket;
+use Sys::Hostname;
 
-require Exporter;
-
-@ISA = qw(Exporter AutoLoader);
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-@EXPORT = qw(
-	
-);
-$VERSION = '0.03';
-
+$VERSION = '0.04';
 
 # Preloaded methods go here.
 
-my %syslog_priorities=(
-	emerg   => 0,
-	alert   => 1,
-	crit    => 2,
-	err     => 3,
-	warning => 4,
-        notice  => 5,
-        info    => 6,
-	debug   => 7
+my %syslog_priorities = (
+    emerg         => 0,
+    emergency     => 0,
+    alert         => 1,
+    crit          => 2,
+    critical      => 2,
+    err           => 3,
+    error         => 3,
+    warning       => 4,
+    notice        => 5,
+    info          => 6,
+    informational => 6,
+    debug         => 7
 );
 
-my %syslog_facilities=(
-	kern	=> 0,
-	user	=> 1,
-	mail	=> 2,
-	daemon	=> 3,	
-	auth	=> 4,
-	syslog	=> 5,
-	lpr	=> 6,
-	news 	=> 7,
-	uucp 	=> 8,
-	cron	=> 9,
-	authpriv=> 10,
-	ftp	=> 11,
-	local0	=> 16,
-	local1	=> 17,
-	local2	=> 18,
-	local3	=> 19,
-	local4	=> 20,
-	local5	=> 21,
-	local6	=> 22,
+my %syslog_facilities = (
+    kern      => 0,
+    kernel    => 0,
+    user      => 1,
+    mail      => 2,
+    daemon    => 3,
+    system    => 3,
+    auth      => 4,
+    syslog    => 5,
+    internal  => 5,
+    lpr       => 6,
+    printer   => 6,
+    news      => 7,
+    uucp      => 8,
+    cron      => 9,
+    clock     => 9,
+    authpriv  => 10,
+    security2 => 10,
+    ftp       => 11,
+    FTP       => 11,
+    NTP       => 11,
+    audit     => 13,
+    alert     => 14,
+    clock2    => 15,
+    local0    => 16,
+    local1    => 17,
+    local2    => 18,
+    local3    => 19,
+    local4    => 20,
+    local5    => 21,
+    local6    => 22,
+    local7    => 23,
 );
 
+my @month = qw{Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec};
 
-sub new{
-  my $class = shift;
-  my $name = $0;
-  if($name =~ /.+\/(.+)/){
-     $name = $1;
-  }
-  my $self = { Name     => $name,
-               Facility => 'local5',
-               Priority => 'err',
-               SyslogPort    => 514,
-               SyslogHost    => '127.0.0.1'};
-  bless $self,$class;
-  my %par = @_;
-  foreach (keys %par){
-    $self->{$_}=$par{$_};
-  }
-  return $self;
+sub new {
+    my $class = shift;
+    my $name  = $0;
+    if ( $name =~ /.+\/(.+)/ ) {
+        $name = $1;
+    }
+    my $self = {
+        Name       => $name,
+        Facility   => 'local5',
+        Priority   => 'error',
+        Pid        => $$,
+        SyslogPort => 514,
+        SyslogHost => '127.0.0.1'
+    };
+    bless $self, $class;
+    my %par = @_;
+    foreach ( keys %par ) {
+        $self->{$_} = $par{$_};
+    }
+    return $self;
 }
 
-sub send{
-  my $self = shift;
-  my $msg=shift;
-  my %par = @_;
-  my %local=%$self;
-  foreach (keys %par){
-    $local{$_}=$par{$_};
-  }
+sub send {
+    my $self  = shift;
+    my $msg   = shift;
+    my %par   = @_;
+    my %local = %$self;
+    foreach ( keys %par ) {
+        $local{$_} = $par{$_};
+    }
 
-  my $pid=$$;
-  my $facility_i=$syslog_facilities{$local{Facility}};
-  my $priority_i=$syslog_priorities{$local{Priority}};  
+    my $pid = ( $local{Pid} =~ /^\d+$/ ) ? "\[$local{Pid}\]" : "";
+    my $facility_i = $syslog_facilities{ $local{Facility} } || 21;
+    my $priority_i = $syslog_priorities{ $local{Priority} } || 3;
 
-  if(!defined $facility_i){
-    $facility_i=21;
-  }
-  if(!defined $priority_i){
-    $priority_i=4;
-  }
+    my $d = ( ( $facility_i << 3 ) | ($priority_i) );
 
-  my $d=(($facility_i<<3)|($priority_i));
-  my $message = "<$d>$local{Name}\[$pid\]: $msg";
+    my $host = inet_ntoa( ( gethostbyname(hostname) )[4] );
+    my @time = localtime();
+    my $ts =
+        $month[ $time[4] ] . " "
+      . ( ( $time[3] < 10 ) ? ( " " . $time[3] ) : $time[3] ) . " "
+      . ( ( $time[2] < 10 ) ? ( "0" . $time[2] ) : $time[2] ) . ":"
+      . ( ( $time[1] < 10 ) ? ( "0" . $time[1] ) : $time[1] ) . ":"
+      . ( ( $time[0] < 10 ) ? ( "0" . $time[0] ) : $time[0] );
+    my $message = '';
 
-  my $sock=new IO::Socket::INET(PeerAddr => $local{SyslogHost},
-                        PeerPort => $local{SyslogPort},
-                        Proto    => 'udp');
-  die "Socket could not be created : $!\n" unless $sock;
-  print $sock $message;
-  $sock->close();
+    if ( $local{rfc3164} ) {
+        $message = "<$d>$ts $host $local{Name}$pid: $msg";
+    }
+    else {
+        $message = "<$d>$local{Name}$pid: $msg";
+    }
+
+    my $sock = new IO::Socket::INET(
+        PeerAddr => $local{SyslogHost},
+        PeerPort => $local{SyslogPort},
+        Proto    => 'udp'
+    );
+    die "Socket could not be created : $!\n" unless $sock;
+    print $sock $message;
+    $sock->close();
 }
-
-
-
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
 
@@ -133,16 +154,25 @@ send call.  Keys (listed with default values) are:
 
 	Name		<calling script name>
 	Facility 	local5
-	Priority 	err
+	Priority 	error
+	Pid		$$
 	SyslogPort    	514
 	SyslogHost    	127.0.0.1
 
 Valid Facilities are:
-	kern, user, mail, daemon, auth, syslog, lpr, news, uucp, cron, 
-	authpriv, ftp, local0, local1, local2, local3, local4, local5, local6
+  kernel, user, mail, system, security, internal, printer, news,
+  uucp, clock, security2, FTP, NTP, audit, alert, clock2, local0,
+  local1, local2, local3, local4, local5, local6, local7
 
 Valid Priorities are:
-	emerg, alert, crit, err, warning, notice, info, debug
+  emergency, alert, critical, error, warning, notice, informational,
+         debug
+
+Set Pid to any non numeric value to disable in the output.
+
+Use:
+         rfc3164 => 1
+ to enable RFC 3164 messages including timestamp and hostname.
 
 
 
